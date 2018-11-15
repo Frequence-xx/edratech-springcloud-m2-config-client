b233f54963f0d746cb1e4b9ea3d52f3481d37968
Edgar Kirchner <edgar.kirchner@gmail.com>
2018-11-15 01:56:55 +0100

    first commit

---
+++ src/main/java/nl/edratech/EdratechSpringcloudM2ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package nl.edratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdratechSpringcloudM2ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdratechSpringcloudM2ConfigClientApplication.class, args);
+    }
+}
